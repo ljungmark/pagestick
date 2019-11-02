c9804a624e57fc5a89127b4035e0d5486929298b
ljungmark <mattias@ljungmark.co>
2019-11-02 21:36:12 +0100

    Split matchMedia mocking into separate file

--- tests/pagestick.test.js
+++ tests/pagestick.test.js
@@ -1,15 +1,4 @@
-window.matchMedia = jest.fn().mockImplementation(query => {
-    return {
-        matches: false,
-        media: query,
-        onchange: null,
-        addListener: jest.fn(), // deprecated
-        removeListener: jest.fn(), // deprecated
-        addEventListener: jest.fn(),
-        removeEventListener: jest.fn(),
-        dispatchEvent: jest.fn(),
-    };
-});
+require('./mocks/matchMedia.mock');
 
 const Pagestick = require('../dist/pagestick').Pagestick;
 
